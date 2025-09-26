609a976090e1f6e93885c88cdb1bdc8f42d0d10e
pedro <AE.lllupus@gmail.com>
2025-09-26 10:51:45 -0300

    main and setting

---
+++ setting.py
@@ -0,0 +1,11 @@
+'''Setting Class Module'''
+
+class Settings():
+    '''A class to storage all configs of 
+    Alien Invasion Project'''
+    def __init__(self):
+        '''Starting game configs'''
+        #Screen Config
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
